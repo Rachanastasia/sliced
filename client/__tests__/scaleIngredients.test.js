8fb9ca5b62c2b20f385ac98e45603631ab3c2271
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-21 15:29:40 -0500

    two tests passing

--- client/__tests__/scaleIngredients.test.js
+++ client/__tests__/scaleIngredients.test.js
@@ -1,5 +1,12 @@
 import { scaleIngredient } from '../utils/scaleIngredient'
+import {UNIT_DATA} from '../config/constants'
+
+test('returns the same values when the constant is 1', ()=> {
+    const INPUT = {amount: {value: 1}, unit: UNIT_DATA.CUP}
+    expect(scaleIngredient(INPUT)).toStrictEqual(INPUT)
+})
+
 
 test('scales amount for ingredients with no unit', ()=> {
-    expect(scaleIngredient({amount: {value: 1}, unit: {}}, 2)).toBe({amount:{value: 2}, unit: {}})
+    expect(scaleIngredient({amount: {value: 1}, unit: {}}, 2)).toStrictEqual({amount:2, unit: {}})
 })
