f942e7e999be9a9941d2d963311f1150e4b5faed
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-21 18:16:00 -0500

    adds file for parsing test

---
+++ client/__tests__/transformInputIntoIngredientData.test.js
@@ -0,0 +1,7 @@
+import {transformInputIntoIngredientData} from '../utils/transformInputIntoIngredientData'
+import sample from '../utils/sampleRecipe'
+
+
+//how do I want to test this to make sure the app works properly? 
+
+//black box test with input??
