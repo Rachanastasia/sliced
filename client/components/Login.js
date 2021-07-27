1494853975faf5ea7a9a3ce0becb95a785588186
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-26 19:24:19 -0500

    adds temp content to login and registration pages

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import Link from 'next/link'
-import Button from './Button';
+import styles from '../styles/Login.module.css'
 
 function Login() {
     return (
-            <section className='Login_section'>
-                    <form
+            <section className={styles.temp_block}>
+                <h2>Sliced has a new login system in progress</h2>
+                <span>To try out Sliced without saving your reipes, use the Slicer <Link href='/#slicer'>here</Link>. </span>
+                    {/* <form
                         autoComplete='off'
                         className='Login_form'>
                         <fieldset>
                             <label className='hidden' htmlFor='email'></label>
-                            <input name='email' type='text' placeholder='test@gmail.com' />
+                            <input name='email' type='text'/>
                         </fieldset>
                         <fieldset>
                             <label className='hidden' htmlFor='password'></label>
-                            <input name='password' type='password' placeholder='test-password' />
+                            <input name='password' type='password' />
                         </fieldset>
                         <div className='form-login-button-wrapper'>
-                            <Button />
+                            <Button disabled />
                             <Link href='/register'>
                                 <span>Create an account</span>
                             </Link>
                         </div>
 
-                    </form>
+                    </form> */}
                 </section>
 
     )
