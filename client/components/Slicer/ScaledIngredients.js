b29dc0533c688da00bafc78ca576b560ab2c9304
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-26 20:48:30 -0500

    fix focus effects and responsiveness

--- client/components/Slicer/ScaledIngredients.js
+++ client/components/Slicer/ScaledIngredients.js
@@ -47,7 +47,7 @@ function SelectScaleConstant({constant, setConstant}){
             <form>
               <input type='range' value={constant}  min={min} step={'any'} max={max} list="options" onChange={(e)=>setConstant(e.target.value)} />
               <datalist id="options">
-                  {SELECT_FROM.map((item)=><option value={item} id={item} label={`${item}`}>{item}</option>)}
+                  {SELECT_FROM.map((item)=><option value={item} key={item} id={item} label={`${item}`}>{item}</option>)}
               </datalist>
               </form>
           </div>
