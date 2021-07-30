b2bd00873fcac579fd155abb762a5898bc4c1c39
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-30 12:38:44 -0500

    adds author meta tag

--- client/components/layout.js
+++ client/components/layout.js
@@ -12,13 +12,14 @@ export default function Screen({children, description=DEFAULT_DESCRIPTION, title
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <title>{title}</title>
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" key='ogtype'/>
+        <meta name="author" content="Rachel Reilly"/>
         <meta property="og:url" content='https://sliced-rachanastasia.vercel.app/' key="ogurl" />
         <meta name="description" property="og:description" content={description} key="ogdesc"/>
         <meta name="title" property="og:title" content={title} key="ogtitle"/>
-        <meta name="image" property="og:image" content="/public/main-page.png" key="ogimage"/>
+        <meta name="image" property="og:image" content="../public/main-page.png" key="ogimage"/>
         <meta property="og:site_name" content='Sliced' key="ogsitename" />
-        <link rel="icon" type="image/x-icon"  href="../public/favicon_io/favicon.ico" />
+        <link rel="icon" type="image/x-icon"  href={favicon} />
       </Head>
       <main>
         <Header />
