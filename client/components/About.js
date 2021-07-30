844ea9b6781a6720824146634830e8b9445e175b
Rachel Reilly <r.a.reill18@gmail.com>
2021-07-30 10:17:46 -0500

    fixes about copy and structure

--- client/components/About.js
+++ client/components/About.js
@@ -1,21 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../styles/About.module.css'
 
 export default function About(){
 
     return (
         <div id='about' className={styles.about_wrapper}>
-            <h2 className={styles.about_subtitle}>What is Sliced?</h2>
-            <p className={`${styles.about_p} ${styles.margin_bottom}`}>Sliced was conceptualized during the pandemic. I had a lot of time to cook, but I was living alone. To avoid food waste</p>
-            <p className={`${styles.about_p} ${styles.margin_bottom}`}>Sliced is an app for scaling your recipes. It scales the ingredients up and down between 1/4 and 4 times the original amount. Common units, including as tablespoons, teaspoons, and cups scale with the amount.</p>
-
-            <h2 className={styles.about_subtitle}>How can I use Sliced?</h2>
-            <p className={styles.about_p}>Just type out one of your recipes or copy and paste it below. The slider beneath the textbox lets you select the amount to scale.</p>
-
-
-            <h2>How precise are the Sliced conversions?</h2>
-
-            <h2>What are the limitations of Sliced recipe conversion?</h2>
+            <h2 className={styles.about_subtitle}>About</h2>
+  
+            <p className={styles.about_p}>With Sliced, you can <em>half</em>, <em>quarter</em>, or <em>two and a half</em> your recipes. Just copy one of your favorite recipes and paste it in the textbox. Use the slider to scale the ingredient amounts. It works by parsing your recipe's ingredients and splitting the ingredient name, amount, and unit. To make measuring easier, some units -- including teaspoons, tablespoons, and cups -- are scaled up and down with the amount.</p>
+            <p className={styles.about_p}>I developed Sliced during the pandemic when I was always cooking for one or two. I kept asking Siri to make calculations then having her convert those to smaller units. I saw recipe sites that had tools to scale up recipes, but I didn't see any tools to scale down recipes. I made Sliced to help people scale their recipes all at once and have that conversion at hand. I hope Sliced helps people adapt their recipes to the amount of food they want to make.</p>
+            <p className={`${styles.about_p} ${styles.about_name}`}> - <Link href='https://github.com/rachelrly'>Rachel Reilly</Link>, Software Engineer</p>
         </div>
     )
 }
