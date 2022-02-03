8ab157a00c93fcd80c3a5d182b245d1233132b21
Rachel <rachelreillydev@gmail.com>
2022-02-02 21:28:39 -0600

    blurs input on click

--- client/components/elements/Input.js
+++ client/components/elements/Input.js
@@ -1,5 +1,13 @@
 import { forwardRef } from 'react'
 
 export const Input = forwardRef(function Input({ text, onBlur }, ref) {
-  return <input ref={ref} placeholder={text} onBlur={onBlur} autoFocus />
+  return (
+    <input
+      ref={ref}
+      placeholder={text}
+      onBlur={onBlur}
+      onClick={onBlur}
+      autoFocus
+    />
+  )
 })
