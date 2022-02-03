f67e43f2a671096af1dfd8eb429b73a8dbd980e9
Rachel <rachelreillydev@gmail.com>
2022-02-03 11:36:24 -0600

    fixes vertical spacing of lines

--- client/components/elements/Dropdown.js
+++ client/components/elements/Dropdown.js
@@ -1,13 +1,18 @@
 import styles from '../../styles/modules/ButtonToInput.module.css'
 
 export function Dropdown({ name, options, onChange }) {
+  // return (
+  //   <select name={name} className={styles.dropdown_wrapper} onChange={onChange}>
+  //     {options.map((option, index) => (
+  //       <option key={index} value={option}>
+  //         {option}
+  //       </option>
+  //     ))}
+  //   </select>
+  // )
   return (
-    <select name={name} className={styles.dropdown_wrapper} onChange={onChange}>
-      {options.map((option, index) => (
-        <option key={index} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <div className={styles.dropdown_wrapper}>
+      <p>{options[0]}</p>
+    </div>
   )
 }
