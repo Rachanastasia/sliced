b63826b79bfae45e51a6f30ad45ef75df9c21400
Rachel <rachelreillydev@gmail.com>
2022-02-07 17:19:25 -0600

    adds shortened amount

--- client/components/About.js
+++ client/components/About.js
@@ -8,26 +8,13 @@ export function About() {
       <h2 className={styles.about_subtitle}>About Sliced</h2>
       <p className={styles.about_p}>
         I first conceptualized Sliced at the beginning of the pandemic when I
-        was cooking for 2 and making do with what I had in the kitchen. I kept
-        asking Siri <em>whats half of 1/3 cups in tablespoons</em> and manually
-        scaling my recipe to the lowest common denominator of the ingredients I
-        had on-hand. Even with food more readily accessable, the need for sliced
-        continues.
-      </p>
-      <p className={styles.about_p}>
-        Traditionally, home recipes are scaled to feed the neucular family. That
-        leaves those of us without 2.5 kids and a husband with a lot of
-        leftovers.
-      </p>
-      <p className={styles.about_p}>
-        With Sliced, home cooks can scale their recipes up or down to accomidate
-        amount of people they are feeding or the amount of ingredients they
-        have. Sliced hopes to bring XXX in a world where even recipes are
-        pushing you to subscribe to societal norms.
-      </p>
-      <p className={styles.about_p}>
-        The modern woman isn't cooking for the neucular family, she's cooking an
-        app to eliminate food waste and fighting against societal norms.✌️
+        was cooking for 2 and making do with what I had in the kitchen most
+        days. To convert recipes to the lowest common denominator of what I had
+        on-hand, I kept asking Siri{' '}
+        <em>whats half of 1/3 cups in tablespoons</em> . Since these
+        calculations are more quickly done by a computer, I created Sliced to
+        scale down amounts and units, and provide a visual reference of the new
+        ingredients.
       </p>
       <span className={styles.about_byline}>
         - Rachel Reilly, creator of Sliced
@@ -35,51 +22,3 @@ export function About() {
     </div>
   )
 }
-
-// the app, the measurements, the creator
-
-// enable creative cooking
-// eliminate food waste
-// selective ingredient adding
-
-// making do with what I had in the kitchen and manually scaling recipes to what I had in the kitchen
-
-/**
- * Sliced was first conecptualized at the beginning of the pandemic when I was cooking for two and making do with what I had
- * in the kitchen.
- *
- * I had nothing but time and would scale down
- */
-
-// Traditional recipes are scaled for the neucular family
-// As people continue to differ in how they live,
-// Can cope with food waste
-// Which means that people
-
-// Recipes are scaled for the neucular family, which leaves those of us without children with excess leftovers
-
-// Rather
-
-// I don't always cook with a recipe, but when I do, I use Sliced
-
-// How limiting it is to have recipes scaled for the neucular family.
-
-// Sliced allows users to copy and paste recipes from elsewhere and adapt those ingredients as well as scale them.
-// Users can exclude ingredients from the scale by unlocking them or
-
-// recipes are scaled to feed the neucular family, not the rest of us
-// mealkits basicaly just shop and plan for you
-
-// The default measurement for recipes is scaled to the neucular familiy, leaving those of us without a husband and 2.5 kids with lots of leftovers.
-//
-// which puts people without children or couple-friends in a predicament
-// For those of us without a husband and 2.5 kids,
-//
-
-// As we enbody difference in our lives, our culinary needs change.
-//
-
-/**
- * Sliced originated from the desire to take recipes scaled by default to the neucular family and adapting their measurements to
- * how many mouths to feed
- */
